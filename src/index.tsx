58996a453285ccb378791055f78173af888d49e4
trieu nguyen <trieunm@liftsoft.vn>
2023-12-06 09:30:50 +0700

    (fix): remove scroll input number

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { NextUIProvider } from "@nextui-org/react";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
@@ -16,8 +16,8 @@ root.render(
   <React.StrictMode>
     <NextUIProvider>
       <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
         </QueryClientProvider>
       </BrowserRouter>
     </NextUIProvider>
