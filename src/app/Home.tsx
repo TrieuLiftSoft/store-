684a51cb92599b93ca340c28397ad6588be8399f
trieu nguyen <trieunm@liftsoft.vn>
2023-12-07 11:39:29 +0700

    (create store Zustand ): setup store and create value

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import CardApp from "../components/CardApp";
-import { useQuery } from "react-query";
-
-const fetchData = async () => {
-  const response = await fetch("https://dummyjson.com/products");
-  const data = await response.json();
-  return data;
-};
+import useItemStore from "../store/ZustandStore";
+import { useProductsAllQuery } from "../api/useQueryProduct";
 
 const Home = () => {
-  const { data, error, isLoading }: any = useQuery("myData", fetchData);
-
+  const { data: ProductsAll, isLoading, error } = useProductsAllQuery();
+  const totalQuantity = useItemStore((state: any) => state.getTotalQuantity());
   if (isLoading) {
     return <p>Loading...</p>;
   }
-
   if (error) {
     return <p>Error: {error.message}</p>;
   }
   return (
     <div>
-      <CardApp data={data} />
+      {ProductsAll ? (
+        <>
+          <p className="text-red-500">Quantity: {totalQuantity}</p>
+          <CardApp data={ProductsAll} />
+        </>
+      ) : (
+        <p>No products available</p>
+      )}
     </div>
   );
 };
