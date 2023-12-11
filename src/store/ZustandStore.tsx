c22dc5bfba70cdb330a4af61eb209cab419c45bd
trieu nguyen <trieunm@liftsoft.vn>
2023-12-11 15:26:17 +0700

    (Create store ): create store addtoCart , delet , totalQuantity , totalPrice ,getQuantity

--- src/store/ZustandStore.tsx
+++ src/store/ZustandStore.tsx
@@ -1,24 +1,47 @@
 // itemStore.ts
 import { create } from "zustand";
+import { CartItem, CartStore } from "../model/InitStore";
+import produce from "immer";
 
-interface Item {
-  id: number;
-  name: string;
-}
-
-interface ItemStore {
-  items: Item[];
-  addItem: (name: string) => void;
-}
-
-const useItemStore: any = create<ItemStore>((set) => ({
-  items: [],
-
-  addItem: (name) => {
-    const id = Date.now();
-    const newItem: Item = { id, name };
-    set((state) => ({ items: [...state.items, newItem] }));
+const useStoreProduct: any = create<CartStore>((set) => ({
+  cart: [],
+  deleteToCart: (productId) =>
+    set((state) => ({
+      cart: state.cart.filter(
+        (item: CartItem) => item.product.id !== productId && item.quantity > 0,
+      ),
+    })),
+  addToCart: (product, quantity) =>
+    set(
+      produce((state) => {
+        const existingItem = state.cart.find(
+          (item: CartItem) => item.product.id === product.id,
+        );
+        if (existingItem) {
+          existingItem.quantity += quantity;
+        } else {
+          state.cart.push({ product, quantity });
+        }
+      }),
+    ),
+  getQuantity: (productId) => {
+    const item: CartItem = useStoreProduct
+      .getState()
+      .cart.find((cartItem: CartItem) => cartItem.product.id === productId);
+    return item ? item.quantity : 0;
   },
+  getTotalQuantity: () =>
+    useStoreProduct
+      .getState()
+      .cart.reduce((total: number, item: CartItem) => total + item.quantity, 0),
+  getTotalPrice: () =>
+    useStoreProduct
+      .getState()
+      .cart.reduce(
+        (total: number, item: CartItem) =>
+          total + item.quantity * item.product.price,
+        0,
+      ),
 }));
 
-export default useItemStore;
+export default useStoreProduct;
