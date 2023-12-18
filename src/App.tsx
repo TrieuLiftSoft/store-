db56544539918cc14e35baa37051cf5d950ca204
trieu nguyen <trieunm@liftsoft.vn>
2023-12-18 08:37:09 +0700

    (fix): update router reload page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./app/Home";
 import NotFound from "./app/NotFound";
+import PaginationScreen from "./app/PaginationScreen";
+import About from "./app/About";
+import NarBav from "./components/Navbar";
 
 export default function App() {
+  const location = useLocation();
+  const currentPath = location.pathname.split("/");
+  const path = currentPath?.[1];
   return (
     <div>
+      {path === "about" ? null : <NarBav />}
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="page/:pageNumber" element={<PaginationScreen />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
